588e37466f55f647abf36371b4f99f9ea91f4204
Erce Güder <erce_guder@icloud.com>
2023-03-13 22:41:25 +0300

    rm deprecation warning

--- mmcv/__init__.py
+++ mmcv/__init__.py
@@ -9,18 +9,3 @@ from .utils import *
 from .version import *
 from .video import *
 from .visualization import *
-
-# The following modules are not imported to this level, so mmcv may be used
-# without PyTorch.
-# - runner
-# - parallel
-# - op
-# - device
-
-warnings.warn(
-    'On January 1, 2023, MMCV will release v2.0.0, in which it will remove '
-    'components related to the training process and add a data transformation '
-    'module. In addition, it will rename the package names mmcv to mmcv-lite '
-    'and mmcv-full to mmcv. '
-    'See https://github.com/open-mmlab/mmcv/blob/master/docs/en/compatibility.md '
-    'for more details.')
